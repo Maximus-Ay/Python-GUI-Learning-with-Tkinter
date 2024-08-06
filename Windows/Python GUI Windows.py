7574eaacff12fe2705965f091e9a598aa09621dc
Maximus-Ay <thedevelopermax@gmail.com>
2024-08-06 11:20:10 -0700

    Update Python GUI Windows.py

--- Windows/Python GUI Windows.py
+++ Windows/Python GUI Windows.py
@@ -8,3 +8,23 @@
     => A widget consists of GUI elements such as buttons, labels, text boxes and images
     => A window is a container that holds the different widgets
 '''
+
+'''
+    To get started you need to able to first of all import the tkinter module as well as all it has to offer.
+    Then to create a window and display it, there are two things that must be done
+
+    1) You create an instance of the window class (Tk class) in tkinter (This is also known as Instantiation)
+    2) You display the window by using the mainloop method
+'''
+
+from tkinter import * # This will import all the attributes and methods common to the tkinter module
+
+# instantiation
+window = Tk()
+
+# Displaying the window
+window.mainloop()
+
+# Running these two lines of code will create a default window in Tkinter
+# It's as easy as that
+
