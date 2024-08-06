69232ff4fe84b2dc48e8c56cada5172c6729a111
Maximus-Ay <thedevelopermax@gmail.com>
2024-08-06 11:15:44 -0700

    Windows in tkinter

---
+++ Windows/Python GUI Windows.py
@@ -0,0 +1,10 @@
+'''
+    To get started with GUI programming which stands for Graphical user interface, you need to import
+    of tkinter module which is a module for building graphical user interfaces that come directly with python.
+'''
+
+'''
+    Now to continue programming using tkinter, you need to be able to distinguish between windows and widgets.
+    => A widget consists of GUI elements such as buttons, labels, text boxes and images
+    => A window is a container that holds the different widgets
+'''
